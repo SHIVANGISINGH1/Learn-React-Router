3cf1861c479aba3c7c56731e90c3ece71b41ef08
Shivangi Singh <shivangisinghhappy@gmail.com>
2022-07-17 20:48:04 +0530

    more links added to invoices

--- src/routes/invoices.js
+++ src/routes/invoices.js
@@ -1,8 +1,18 @@
+import { Link } from "react-router-dom"
+import { invoices } from "../data"
+import '../invoices.css';
 export default function Invoices() {
 
     return (
-        <div>
-            <h1>Invoices</h1>
+      <div className="main-div">
+        <h1>Invoices</h1>
+        <div className="side-navbar">
+            {invoices.map((inv) => (
+                <Link to={`/invoices/${inv.number}`} key={inv.number}>
+                {inv.name}
+                </Link>
+            ))}
         </div>
-    )
+      </div>
+    );
 } 
